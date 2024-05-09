b72df0bd94f5ad9a333556b9eb02be453e1af623
Israel Sánchez <israelclarinete@gmail.com>
2024-05-09 11:23:54 +0200

    Random Card G incluye Joker

--- src/app.js
+++ src/app.js
@@ -4,8 +4,48 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/diam-treb-removebg-preview.png";
+import "./assets/img/pic-cor-removebg-preview.png";
+import "./assets/img/joker-removebg-preview.png";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardSuit = document.querySelectorAll(".card-suit");
+
+  const cardValue = document.getElementById("card-value");
+
+  const buttonG = document.getElementById("card-generator");
+
+  const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", "Joker"];
+
+  const suits = ["♦", "♥", "♠", "♣"];
+
+  function cardGenerator() {
+    let randomIndexValue = Math.floor(Math.random() * values.length);
+    let randomIndexSuit = Math.floor(Math.random() * suits.length);
+    let randomValue = values[randomIndexValue];
+    let randomSuit = suits[randomIndexSuit];
+    if (randomValue === "Joker") {
+      cardValue.innerHTML =
+        "<img src='./joker-removebg-preview.png' style='height: 380px; margin-top: 15px' />";
+      cardSuit[0].style.display = "none";
+      cardSuit[1].style.display = "none";
+    } else {
+      cardSuit[0].style.display = "block";
+      cardSuit[1].style.display = "block";
+      cardSuit[0].textContent = randomSuit;
+      cardSuit[1].textContent = randomSuit;
+      cardValue.textContent = randomValue;
+      const isRed = randomSuit === "♥" || randomSuit === "♦";
+      if (isRed) {
+        cardSuit[0].style.color = "red";
+        cardSuit[1].style.color = "red";
+      } else {
+        cardSuit[0].style.color = "black";
+        cardSuit[1].style.color = "black";
+      }
+    }
+  }
+
+  buttonG.addEventListener("click", cardGenerator);
 };
